cc4478adb13b0228c9a202aeb48064eb249f521d
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-07-02 13:46:52 +0700

    Q4: Finish Question 4. Create React native CLI apps

--- Q4ReactNative/App.tsx
+++ Q4ReactNative/App.tsx
@@ -1,45 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { ScrollView } from "react-native";
+import styles from "./style";
+import data from './Data';
+import Square from './Square';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <ScrollView style={styles.container}>
+      {data.map((item, index) => (
+        <Square key={item} text={`Square ${index + 1}`} />
+      ))}
+    </ScrollView>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
